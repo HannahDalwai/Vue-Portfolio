a04ebc4621b405b9d243c4bec5949e87e9b1caee
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-25 14:42:32 +0200

    nav styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,14 +8,13 @@
     <div class="d-flex flex-column">
 
       <div class="profile">
-        <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+        <img src="" alt="" class="img-fluid rounded-circle">
         <h1 class="text-light"><a href="index.html">Hannah</a></h1>
         <div class="social-links mt-3 text-center">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <a href="#" class="github"><i class='bx bxl-github'></i></a>
+          <a href="#" class="netlify"><i class='bx bxl-netlify'></i></a>
+          <a href="#" class="linkedin"><i class='bx bxl-linkedin'></i></a>
+          <a href="#" class="codepen"><i class='bx bxl-codepen'></i></a>
         </div>
       </div>
 
