574415b2af5898a4ebb91558a06d8a80e1e2507f
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-25 11:31:05 +0200

    changing projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,25 @@
 <template>
+<Navbar/>
+       <h1>Projects</h1>
+<div v-for="projects in Projects" :key="projects.id" class="projects"></div>
+ <router-link :to="{name:'ProjectDetails', params: { id:projects.is}}"> 
+   <h2>{{ projects.title }}</h2>
+ </router-link>
+ <h2>{{ projects.title }}</h2>
+
             <button @click="redirect">Redirect</button>
             <button @click="back"> Go back</button>
             <button @click="forward">Go Forward</button>
 </template>
 
 <script>
+
+import Navbar from '../components/Navbar.vue'
+
 export default {
+
+  components: {Navbar,},
+
 data() {
     return {
         projects:[]
