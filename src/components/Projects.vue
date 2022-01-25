721b64453865cf5b60349988b8c21f79de72b315
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-25 11:00:03 +0200

    projects/data/fetching

---
+++ src/components/Projects.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        projects:[]
+    }
+},
+1
+
+mounted() {
+    fetch('http://localhost:3000/projects')
+     .then((res)=> res.json())
+      .then(data=>this.projects=data)
+      .catch(err => console.log(err.message))
+}
+}
+</script>
+
+<style>
+
+</style>
