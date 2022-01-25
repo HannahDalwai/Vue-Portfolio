fcc954f2de5b86679fcdb4a7be181a5b6a527b75
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-25 11:07:25 +0200

    moving fetch

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,24 @@
 <template>
-  <button @click="redirect">Redirect</button>
-<button @click="back"> Go back</button>
-<button @click="forward">Go Forward</button>
+            <button @click="redirect">Redirect</button>
+            <button @click="back"> Go back</button>
+            <button @click="forward">Go Forward</button>
 </template>
 
 <script>
 export default {
+data() {
+    return {
+        projects:[]
+    }
+},
+
+mounted() {
+    fetch('http://localhost:3000/projects')
+     .then((res)=> res.json())
+      .then(data=>this.projects=data)
+       .catch(err => console.log(err.message))
+},
+
 methods: {
     redirect(){
        this.$router.push({name:'Home'})
