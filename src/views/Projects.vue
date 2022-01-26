9bf92829beb71828eb699c57e8cf24929116b49e
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-26 15:49:06 +0200

    error fixed

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -23,21 +23,21 @@
 
 export default {
 
-components: {},
+      components: {},
 
-data() {
-    return {
-        projects:[]
-    }
-},
+      data() {
+          return {
+              projects:[]
+          }
+      },
 
-mounted() {
-    fetch('http://localhost:3000/projects')
-     .then((res)=> res.json())
-      .then(data => this.projects=data)
-       .catch(err => console.log(err.message))
+      mounted() {
+          fetch('http://localhost:3000/projects')
+          .then((res)=> res.json())
+            .then(data => this.projects=data)
+            .catch(err => console.log(err.message))
+      }
 }
-
 </script>
 
 <style>
