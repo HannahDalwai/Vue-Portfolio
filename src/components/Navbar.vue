97d554e1bcf1b1faeaa5877e271749c5da1a46b1
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-26 13:42:19 +0200

    styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,17 +4,16 @@
 <!-- NAVBAR -->
             <nav id="navbar" class="nav-menu navbar">
 
-      <div class="profile">
-        <img src="https://i.postimg.cc/pTtYQqTn/fall-g93027d2b4-1920.jpg" alt="" class="img-fluid rounded-circle">
-          <h1 class="text-light"><a href="index.html">Hannah</a></h1>
-            <div class="social-links mt-3 text-center">
-              <a href="#" class="github" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Github"><i class='bx bxl-github'></i></a>
-              <a href="#" class="netlify" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Netlify"><i class='bx bxl-netlify'></i></a>
-              <a href="#" class="linkedin" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Linkedin"><i class='bx bxl-linkedin'></i></a>
-              <a href="#" class="codepen" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Codepen"><i class='bx bxl-codepen'></i></a>
-            </div>
-  </div>
-
+                <div class="profile">
+                  <img src="https://i.postimg.cc/pTtYQqTn/fall-g93027d2b4-1920.jpg" alt="" class="img-fluid rounded-circle">
+                    <h1 class="text-light"><a href="index.html">Hannah</a></h1>
+                      <div class="social-links mt-3 text-center">
+                        <a href="#" class="github" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Github"><i class='bx bxl-github'></i></a>
+                        <a href="#" class="netlify" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Netlify"><i class='bx bxl-netlify'></i></a>
+                        <a href="#" class="linkedin" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Linkedin"><i class='bx bxl-linkedin'></i></a>
+                        <a href="#" class="codepen" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Codepen"><i class='bx bxl-codepen'></i></a>
+                      </div>
+                </div>
                <button class="mobile-nav" @click="openNav()">Open</button>
                   <ul>
                     <li> <router-link to="/"><i class='bx bxs-home' ></i>  Home</router-link>   </li>
@@ -23,7 +22,6 @@
                     <li>   <router-link to="/Contact"><i class='bx bxs-chat'></i>  Contact</router-link> </li>
                   </ul>
             </nav>
-
 <router-view/>
 </template>
 
@@ -38,12 +36,8 @@ methods: {
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  scroll-behavior: smooth;
-}
+
+
 
 nav {
   position: fixed;
@@ -52,7 +46,8 @@ nav {
   bottom: 0;
   background: #222;
   width: 280px;
-  transition: all 0.3s linear;
+  height: 100%;
+  transition: all 0.1s linear;
   padding: 30px 0 0 0;
 }
 /* OTHER NAV STYLING */
@@ -68,7 +63,7 @@ nav > ul > li {
   white-space: nowrap;
 }
 nav a, nav a:focus {
-  display: flex;
+
   align-items: center;
   color: #a8a9b4;
   padding: 12px 15px;
@@ -97,7 +92,7 @@ nav a:hover i, .nav .active i, nav .active:focus i, nav li:hover > a i {
   display: none;
 }
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 900px) {
   nav {
     left: -100%;
   }
@@ -109,32 +104,21 @@ nav a:hover i, .nav .active i, nav .active:focus i, nav li:hover > a i {
   }
   .mobile-nav {
     display: block;
+    
   }
 }
 
 
 
                           /* PROFILE IN NAV */
-/* .header{
-position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 300px;
-  transition: all ease-in-out 0.5s;
-  z-index: 9997;
-  transition: all 0.5s;
-  padding: 0 15px;
-  background: red;
-  overflow-y: auto;
-}
-.header .profile img {
-  margin: 15px auto;
-  display: block;
-  width: 120px;
-  border: 8px solid #2c2f3f;
-}
-.header .profile h1 {
+.profile img {
+    margin: 15px auto;
+    display: block;
+    width: 120px;
+    border: 8px solid #2c2f3f;
+  }
+
+.profile h1 {
   font-size: 24px;
   margin: 0;
   padding: 0;
@@ -143,11 +127,11 @@ position: fixed;
   text-align-last: center;
   font-family: "Poppins", sans-serif;
 }
-.header .profile h1 a, #header .profile h1 a:hover {
+.profile h1 a, #header .profile h1 a:hover {
   color: #fff;
   text-decoration: none;
 }
-.header .profile .social-links a {
+.profile .social-links a {
   font-size: 18px;
   display: inline-block;
   background: #212431;
@@ -161,9 +145,9 @@ position: fixed;
   height: 36px;
   transition: 0.3s;
 }
-.header .profile .social-links a:hover {
+.profile .social-links a:hover {
   background: #149ddd;
   color: #fff;
   text-decoration: none;
-} */
+} 
 </style>
